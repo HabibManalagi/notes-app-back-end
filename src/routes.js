981c90e00897509acc2c0c3d4a5f7e88a7ff9a83
Habib Maulana <habib.allampungi@gmail.com>
2023-02-09 00:04:31 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+const {addNoteHandler, getAddNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAddNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+module.exports = routes;
